a4948205767035300390a7b54fba4e1fa70f11b1
Jorge Leandro <111012573+jorgeleandrolc@users.noreply.github.com>
2022-09-29 12:15:08 +0000

    Add conteúdo do conhecimento

---
+++ conhecimentos.js
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <Template>
+      <h1>Conhecimentos</h1>
+      <p>Linguagens de Progamação - JavaScript, Node, React, Next.</p>
+      <p>Banco de Dados - MongoDB, PostgreSQL, MySQL Workbench</p>
+
+    </Template>
+  );
+}
