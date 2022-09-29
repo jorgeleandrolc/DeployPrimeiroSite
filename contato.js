6dee538d40370aac84c76b7afdc3fe4238504b9e
Jorge Leandro <111012573+jorgeleandrolc@users.noreply.github.com>
2022-09-29 12:16:49 +0000

    Add conteúdo do contato

---
+++ contato.js
@@ -0,0 +1,16 @@
+  const App = () => {
+  return (
+    <Template>
+      <h4>Contato</h4>
+      <p>Instagram: __jorgeleandro</p>
+      <p>Telefone: (67) 996861344</p>
+      <p>Email: jorge.carvalho2@estudante.ifms.edu.br </p>
+      <p></p>
+    </Template>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
