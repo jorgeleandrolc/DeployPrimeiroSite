51066a5f8cd1dcda2d2f0450565d22128734fd01
Jorge Leandro <111012573+jorgeleandrolc@users.noreply.github.com>
2022-09-29 12:29:43 +0000

    Commit final

--- conhecimentos.js
+++ conhecimentos.js
@@ -4,6 +4,7 @@ const App = () => {
       <h1>Conhecimentos</h1>
       <p>Linguagens de Progamação - JavaScript, Node, React, Next.</p>
       <p>Banco de Dados - MongoDB, PostgreSQL, MySQL Workbench</p>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/c/cb/Postgres_Query.jpg" width="250" height="250" />
 
     </Template>
   );
--- contato.js
+++ contato.js
@@ -2,9 +2,12 @@
   return (
     <Template>
       <h4>Contato</h4>
-      <p>Instagram: __jorgeleandro</p>
+      <p>Instagram: jorgeleandrolc</p>
       <p>Telefone: (67) 996861344</p>
       <p>Email: jorge.carvalho2@estudante.ifms.edu.br </p>
+      <p>Linkedin: Jorge Leandro Lima Carvalho</p>
+      <img src="https://d2k7w3fmrpj0w4.cloudfront.net/advices/photos/000/000/798/medium/999c58e98401e287626190d3aff58c48d3411610.jpg?1642017136" width="250" height="250" />
+      
       <p></p>
     </Template>
   );
